f640e50623b46df0a6140e178d1b90e088151899
Hasan <hasansd432@gmail.com>
2025-07-17 10:43:38 +0530

    update fetch and update

--- utils/fetchAndUpdate.js
+++ utils/fetchAndUpdate.js
@@ -11,6 +11,10 @@ module.exports = async function fetchAndUpdate() {
       "https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage.json",
       {
         params: { ts: Date.now() },
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        },
       }
     );
 
