c23ab5da025a2c7ccadf48da00983dabe60db779
Hasan <hasansd432@gmail.com>
2025-07-17 10:49:53 +0530

    update fetch and update2

--- utils/fetchAndUpdate.js
+++ utils/fetchAndUpdate.js
@@ -14,6 +14,7 @@ module.exports = async function fetchAndUpdate() {
         headers: {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+          Accept: "application/json",
         },
       }
     );
